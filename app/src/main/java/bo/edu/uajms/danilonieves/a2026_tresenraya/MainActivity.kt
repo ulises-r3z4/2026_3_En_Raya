dd2151375969b010bb8a3da4023278aed200eecd
ulises-r3z4 <261569106+ulises-r3z4@users.noreply.github.com>
2026-08-27 16:17:38 -0400

    Add files via upload

---
+++ app/src/main/java/bo/edu/uajms/danilonieves/a2026_tresenraya/MainActivity.kt
@@ -0,0 +1,12 @@
+package bo.edu.uajms.danilonieves.a2026_tresenraya
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+
+class MainActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
